9c78d70a3f0d10bc36effe11c4e5d800abe8b8bd
Artem <tema.arttem@yandex.ru>
2023-03-18 07:27:07 +0300

    first commit

---
+++ helllo.c
@@ -0,0 +1,46 @@
+#include <stdio.h> // preprocessor
+
+int main(void)
+{
+
+    /*
+    int goblin_gold = 15;
+    int goblin_hp;
+    int player_gold = 0;
+    char action;
+
+    printf("You attacked byy goblin. 'a'ttack or 'r'unn\n", goblin_hp);
+
+    action = getchar();
+
+    while (action != 'r')
+    {
+
+        if (action == 'a' || 'A')
+        {
+            for (goblin_hp = 3; goblin_hp >= 0; goblin_hp--)
+            {
+                if (goblin_hp != 3)
+                    printf("You kick goblin. Goblin HP: %d \n,", goblin_hp);
+
+                if (goblin_hp == 0 || goblin_hp == 1)
+                {
+                    printf("You defeat it. \n");
+                    player_gold += goblin_gold;
+                    printf("You found %d goblin_gold. You have %d gold total\n", goblin_gold, player_gold);
+                    break;
+                }
+            }
+        }
+
+        printf("What do you next?\n");
+        getchar();
+        action = getchar();
+    }
+
+    if (action == 'r')
+        printf("You've decided to run away!\n");
+
+    return 0;
+    */
+}
